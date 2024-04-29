2199b9ab49219e8520b54ade96834059ae1bf2de
Koupy <nanook.tech@proton.me>
2024-04-29 19:35:05 +0200

    Create ticket.py

---
+++ ticket.py
@@ -0,0 +1,17 @@
+priority = ["faible", "moyen", "élevé", "critique"]
+status = ["ouvert", "en cours", "résolu"]
+
+
+def seeTicket():
+  return "ticket"
+
+def seeTickets():
+  return "tickets"
+
+def choosePriority():
+  return "priority"
+
+def chooseStatus():
+  print("Quel tickets souhaitez vous voir ?")
+
+
