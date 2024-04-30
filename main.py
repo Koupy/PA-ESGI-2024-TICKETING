bffe8897ae9d4aaaea16794adb9184272a2b46d0
Koupy <nanook.tech@proton.me>
2024-04-30 15:17:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ while True:
     menu.exitMenu()
     break
   elif choice == 1:
-    menu.mainMenuCategory()
+    menu.mainMenuPriority()
   elif choice == 2:
-    menu.mainMenuStatus()
+    menu.mainMenuCategory()
   elif choice == 3:
+    menu.mainMenuStatus()
+  elif choice == 4:
     menu.mainMenuAllTickets()
   else:
     print(f"\033[91mOption invalide !\033[0m")
