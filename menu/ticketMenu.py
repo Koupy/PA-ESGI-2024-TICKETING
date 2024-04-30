333fae1b134b7fc05d5e4eb8ac66c4759dc732e0
Koupy <nanook.tech@proton.me>
2024-04-30 15:41:05 +0200

    add menu package

---
+++ menu/ticketMenu.py
@@ -0,0 +1,24 @@
+import menu
+import menu.mainMenu
+
+def categoryTicket():
+  while True:
+    choice = menu.mainMenu.getChoice()
+    
+    if choice == 0:
+      menu.mainMenu.exitMenu()
+      break
+    elif choice == 1:
+      menu.mainMenu.mainMenuPriority()
+    elif choice == 2:
+      menu.mainMenu.mainMenuCategory()
+    elif choice == 3:
+      menu.mainMenu.mainMenuStatus()
+    elif choice == 4:
+      menu.mainMenu.mainMenuAllTickets()
+    elif choice == 5:
+      menu.mainMenu.mainMenuAllTickets()
+    elif choice == 6:
+      menu.mainMenu.mainMenuAllTickets()
+    else:
+      print(f"\033[91mOption invalide !\033[0m")
