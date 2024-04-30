a7df081cc6bcc9b1273f72279e7242f220b175d0
Koupy <nanook.tech@proton.me>
2024-04-30 14:53:33 +0200

    add choosePriority and chooseStatus func in ticket.py

--- ticket.py
+++ ticket.py
@@ -1,17 +1,26 @@
+import requests
+
 priority = ["faible", "moyen", "élevé", "critique"]
-status = ["ouvert", "en cours", "résolu"]
+status = ["Ouvert", "En cours", "Résolu"]
 
 
 def seeTicket():
   return "ticket"
 
 def seeTickets():
-  return "tickets"
+  url = 'http://localhost:5000/api/ticket/'
+  response = requests.get(url)
+  print('Response Content:', response.text)
+
 
 def choosePriority():
-  return "priority"
+  print("Quel tickets souhaitez vous voir ?")
+  for i in range(len(priority)):
+    print(f"{i+1}. {priority[i]} (/nmb_tickets/)")
 
 def chooseStatus():
   print("Quel tickets souhaitez vous voir ?")
+  for i in range(len(status)):
+    print(f"{i+1}. {status[i]} (/nmb_tickets/)")
 
 
