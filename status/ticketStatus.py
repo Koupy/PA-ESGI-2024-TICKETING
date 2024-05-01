b8c2bd5977d3a7e97a65071581daf8f39d6c4fdc
Koupy <nanook.tech@proton.me>
2024-05-01 15:54:46 +0200

    add ticket status display in ticketStatus.py

--- status/ticketStatus.py
+++ status/ticketStatus.py
@@ -1,5 +1,25 @@
 import ticket
+import menu.ticketMenu
 
 def chooseStatus():
     status_map = {1: "ouvert", 2: "en cours", 3: "résolu"}
     ticket.displayChoices('status', status_map)
+    menu.ticketMenu.statusTicket()
+
+def displayTicketsByStatus(status):
+    tickets = ticket.getTickets()
+    for ticket_item in tickets:
+        if ticket_item['status'] == status:
+            print("."*10)
+            print(ticket_item['title'])
+            print(f"Description : {ticket_item['description']}")
+            print("Quel ticket souhaitez vous voir ?")
+
+def openTicket():
+    displayTicketsByStatus("Ouvert")
+
+def ongoingticket():
+    displayTicketsByStatus("En cours")
+
+def closedTicket():
+    displayTicketsByStatus("Fermé")
